eb972d6a66bab35543e259d51e07c3c1b13cbc9d
Jon <jwood803@users.noreply.github.com>
2014-11-11 07:35:31 -0600

    Able to get required data
    Still need to generate it into a frame…

--- Delivery Data/delivery_data.fsx
+++ Delivery Data/delivery_data.fsx
@@ -1,24 +1,34 @@
-﻿#I "../packages/FSharp.Data.2.1.0/lib/net40"
+#I "../packages/FSharp.Data.2.0.14/lib/net40"
 #r "FSharp.Data.dll"
-
 open FSharp.Data
 
 let deliveryData = CsvProvider<"delivery_data.csv">.GetSample()
 
-let grouped = [for row in deliveryData.Rows do
-                yield row.Pickup_zipcode]
-                    |> Seq.countBy id 
-                    |> Seq.filter(fun (z, c) -> c > 20 && z.HasValue) 
-                    |> Seq.map(fun (z, c) -> z.Value)
+let validZipCodes = deliveryData.Rows
+                        |> Seq.map(fun r -> r.pickup_zipcode)
+                        |> Seq.countBy id
+                        |> Seq.filter(fun (z, c) -> c > 20 && z.HasValue)
+                        |> Seq.map(fun (z, c) -> z.Value)
 
-let filterByZipAndPrice (pickupZip: System.Nullable<int>) (inputZip: int) =
-    pickupZip.HasValue && pickupZip.Value = inputZip
+let filterByZip (pickupZip: System.Nullable<int>) inputZip = pickupZip.HasValue && pickupZip.Value = inputZip
 
-let getItemsLessThan60ByZip zip = deliveryData.Rows 
-                                    |> Seq.filter(fun i -> filterByZipAndPrice i.Pickup_zipcode zip)
+let getItemsLessThan60ByZip zip = deliveryData.Rows
+                                    |> Seq.filter(fun i -> (filterByZip i.pickup_zipcode zip) && i.purchase_price < 60m)
+                                    |> Seq.map(fun i -> i.pickup_zipcode)
+                                    |> Seq.countBy id
 
 let getItemsBetween60And120ByZip zip = deliveryData.Rows
-                                        |> Seq.filter(fun i -> i.Pickup_zipcode.HasValue && i.Pickup_zipcode.Value = zip && (i.Purchase_price > 60m && i.Purchase_price < 120m))
+                                        |> Seq.filter(fun i -> (filterByZip i.pickup_zipcode zip) && (i.purchase_price > 60m && i.purchase_price < 120m))
+                                        |> Seq.map(fun i -> i.pickup_zipcode)
+                                        |> Seq.countBy id
 
 let getItemsGreaterThan120ByZip zip = deliveryData.Rows
-                                        |> Seq.filter(fun i -> i.Pickup_zipcode.HasValue && i.Pickup_zipcode.Value = zip && i.Purchase_price >= 120m)
+                                        |> Seq.filter(fun i -> (filterByZip i.pickup_zipcode zip) && i.purchase_price >= 120m)
+                                        |> Seq.map(fun i -> i.pickup_zipcode)
+                                        |> Seq.countBy id
+
+validZipCodes |> Seq.collect(fun i -> getItemsLessThan60ByZip i) |> Seq.toList
+validZipCodes |> Seq.collect(fun i -> getItemsBetween60And120ByZip i) |> Seq.toList
+validZipCodes |> Seq.collect(fun i -> getItemsGreaterThan120ByZip i) |> Seq.toList
+
+// Need to use Deedle to now create a frame to display the data (refactor a bit of the above to completely use Deedle or just use for the frame).
